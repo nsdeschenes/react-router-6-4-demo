0496c1bd52e58ed004e27a23dcf6aec96be47240
nsdeschenes <nsdeschenes@outlook.com>
2022-09-14 19:00:34 -0300

    add route to pokemon

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Home: React.FC = () => {
   return (
     <div>
       <h1 className="text-2xl font-semibold">Home</h1>
+      <Link to="pokemon">
+        <span className="text-xl text-blue-600 hover:underline">Pokemon</span>
+      </Link>
     </div>
-  )
-}
+  );
+};
